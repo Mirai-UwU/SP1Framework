53a27d6a36ebd5351d922869ef471a4ed0cf436f
EvanQP <69576334+EvanQP@users.noreply.github.com>
2020-08-19 17:46:31 +0800

    Small fog of war

---
+++ SP1Framework/Small_fog_of_war.cpp
@@ -0,0 +1,40 @@
+#include "Small_fog_of_war.h"
+
+
+extern SGameChar g_sChar;
+
+int Small_fog_of_war::rec1_coords()
+{
+	//get player coords 
+	//Assign player x coords to x
+	//Assign player y coords to y
+
+	// Set the width and length
+	width = 5;
+	length = 3;
+	
+	width2 = width / 2;
+	length2 = length / 2;
+
+	return 0;
+
+}
+
+int Small_fog_of_war::rec2_coords()
+{
+	//get player coords 
+	//Assign player x coords to x
+	//Assign player y coords to y
+
+	// Set the width and length
+	width = 5;
+	length = 3;
+
+	width2 = width / 2;
+	length2 = length / 2;
+
+	return 0;
+	
+}
+
+
---
+++ SP1Framework/Small_fog_of_war.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include "game.h"
+
+class Small_fog_of_war
+{
+private:
+	int width, length, x, y, width2, length2;
+public:
+	int rec1_coords();
+	int rec2_coords();
+
+};
+
